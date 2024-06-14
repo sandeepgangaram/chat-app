bff041331bb7a4a557eef623ffba034df37aca30
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2024-06-14 07:57:12 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(8080, () => {
+  console.log("Listening on port 8080");
+});
