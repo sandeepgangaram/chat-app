0a7db579b2f0876b0e39c46aa4c20a9c242d4d5d
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2024-06-15 19:30:08 +0530

    consume avatars, add fontawesome icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,32 @@ import Register from "./components/Auth/Register";
 import Chat from "./components/Chat/Chat";
 import ProtectedRoute from "./components/Router/protectedRoute";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSmile, faImage } from "@fortawesome/free-regular-svg-icons";
+import {
+  faSpinner,
+  faEllipsisV,
+  faUserPlus,
+  faSignOutAlt,
+  faTrash,
+  faCaretDown,
+  faUpload,
+  faTimes,
+  faBell,
+} from "@fortawesome/free-solid-svg-icons";
+library.add(
+  faSmile,
+  faImage,
+  faSpinner,
+  faEllipsisV,
+  faUserPlus,
+  faSignOutAlt,
+  faTrash,
+  faCaretDown,
+  faUpload,
+  faTimes,
+  faBell
+);
 function App() {
   return (
     <Router>
--- frontend/src/components/Chat/Navbar/Navbar.js
+++ frontend/src/components/Chat/Navbar/Navbar.js
@@ -1,4 +1,6 @@
 import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import "./Navbar.scss";
 
 const Navbar = () => {
@@ -7,10 +9,11 @@ const Navbar = () => {
     <div id="navbar">
       <h2>Chat.io</h2>
       <div id="profile-menu">
+        <img width="40" height="40" src={user.avatar} alt="Avatar" />
         <p>
           {user.firstName} {user.lastName}
         </p>
-        <img src="" alt="Avatar" />
+        <FontAwesomeIcon icon="caret-down" className="fa-icon" />
       </div>
     </div>
   );
