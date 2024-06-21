2ee7944f1ebab24b881effdbaa5f98062ae8591e
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2024-06-21 21:58:19 +0530

    online status race condition bug fix

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,11 +1,11 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Navbar from "./components/Navbar/Navbar";
 import FriendList from "./components/FriendList/FriendList";
 import Messenger from "./components/Messenger/Messenger";
 import "./Chat.scss";
 import useSocket from "./hooks/socketConnect";
-import { fetchChats } from "../../store/actions/chat";
+// import { fetchChats } from "../../store/actions/chat";
 
 const Chat = () => {
   const dispatch = useDispatch();
@@ -13,11 +13,11 @@ const Chat = () => {
 
   useSocket(user, dispatch);
 
-  useEffect(() => {
-    dispatch(fetchChats())
-      .then((res) => console.log(res))
-      .catch((e) => console.log(e));
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(fetchChats())
+  //     .then((res) => console.log(res))
+  //     .catch((e) => console.log(e));
+  // }, [dispatch]);
   return (
     <div id="chat-container">
       <Navbar />
--- frontend/src/components/Chat/hooks/socketConnect.js
+++ frontend/src/components/Chat/hooks/socketConnect.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { io } from "socket.io-client";
 import {
+  fetchChats,
   setFriendOffline,
   setFriendOnline,
   setFriendsOnline,
@@ -8,28 +9,34 @@ import {
 
 function useSocket(user, dispatch) {
   useEffect(() => {
-    const socket = io("http://localhost:8080");
+    dispatch(fetchChats())
+      .then((res) => {
+        const socket = io("http://localhost:8080");
 
-    socket.emit("join", user);
+        socket.emit("join", user);
 
-    socket.on("typing", (user) => {
-      console.log("event", user);
-    });
+        socket.on("typing", (user) => {
+          console.log("event", user);
+        });
 
-    socket.on("friends", (friends) => {
-      dispatch(setFriendsOnline(friends));
-      console.log("Friends", friends);
-    });
+        socket.on("friends", (friends) => {
+          dispatch(setFriendsOnline(friends));
+          console.log("Friends", friends);
+        });
 
-    socket.on("online", (friend) => {
-      dispatch(setFriendOnline(friend));
-      console.log("online", friend);
-    });
+        socket.on("online", (friend) => {
+          dispatch(setFriendOnline(friend));
+          console.log("online", friend);
+        });
 
-    socket.on("offline", (friend) => {
-      dispatch(setFriendOffline(friend));
-      console.log("offline", friend);
-    });
+        socket.on("offline", (friend) => {
+          dispatch(setFriendOffline(friend));
+          console.log("offline", friend);
+        });
+
+        console.log(res);
+      })
+      .catch((e) => console.log(e));
   }, [dispatch, user]);
 }
 
